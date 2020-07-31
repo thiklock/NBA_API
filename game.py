ef2da47b56192e64d7acef3c4614de9e9ce82ba3
thiago.klock@indicium.tech <thiago.klock@indicium.tech>
2020-07-30 23:24:17 -0300

    game.py

--- game.py
+++ game.py
@@ -4,30 +4,40 @@ from nba_api.stats.endpoints import LeagueGameFinder
 
 nba_teams = teams.get_teams()
 
+class create_table:
+    def __init__(table, name, id, date):
+        table.name = name
+        table.id = id
+        table.date = date 
 
+    def print_para(abc):
+        print("The table's name is:" + abc.name )
+
+p1 = create_table("table", "name", "date")
+
+p1.print_para()
 # Input for the Team
-
 # Team_id = print(input('Type the teams abbreviation: '))
 
 # Getting the ID for the Team
-Mavericks = [team for team in nba_teams if team['abbreviation'] == 'DAL' ][0]
+# Mavericks = [team for team in nba_teams if team['abbreviation'] == 'DAL' ][0]
+# 
+# Mavericks_id = Mavericks['id']
 
-Mavericks_id = Mavericks['id']
-
-print(f'Mavs Id is: {Mavericks_id}')
+# print(f'Mavs Id is: {Mavericks_id}')
 
 # Catching games
-gamefinder = LeagueGameFinder(team_id_nullable=Mavericks_id)
+# gamefinder = LeagueGameFinder(team_id_nullable=Mavericks_id)
 
 # A DataFrame with those games.
-Team_Games = gamefinder.get_data_frames()[0]
+# Team_Games = gamefinder.get_data_frames()[0]
 # print(Team_Games.tail(20))
 # Saving the DataFrame into a .csv file so we can check it. 
-Team_Games.to_csv(r'/home/tiktek/nba/nba.csv')
+# Team_Games.to_csv(r'/home/tiktek/nba/nba.csv')
 
-Number_of_games = int(Team_Games[['TEAM_ID']].count())
-print(f'There is {Number_of_games} games in the DataFrame')
-
-
-Games_Season = Team_Games[['SEASON_ID']  == 22019]
-Games_Season.to_csv(r'/home/tiktek/nba/Seasongames.csv')
+# Number_of_games = int(Team_Games[['TEAM_ID']].count())
+# print(f'There is {Number_of_games} games in the DataFrame')
+# 
+# 
+# Games_Season = Team_Games[['SEASON_ID']  == 22019]
+# Games_Season.to_csv(r'/home/tiktek/nba/Seasongames.csv')
