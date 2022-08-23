1f55e708fdc5e68a2ee497eecb5cf3b30f8b340e
Thiago Klock <thiago.klock@n26brasil.com>
2022-08-22 22:36:57 -0300

    deletes unused cohort ftmau mau

--- spurs_games.py
+++ spurs_games.py
@@ -1,6 +1,10 @@
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 
+import time
+import logging
+
+logging.basicConfig(filename='spurs_games.log', level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S', format='%(asctime)s %(levelname)-8s %(message)s' )
 
 def main():
     nba_teams = teams.get_teams()
@@ -11,8 +15,12 @@ def main():
     gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=spurs_id)
     # The first DataFrame of those returned is what we want.
     games = gamefinder.get_data_frames()[0]
-    print(games.head())
-    print(spurs_id)
+    games_in_request = len(games)
+    logging.info(f'Games read: {games_in_request}')
+    games.to_csv("./spurs_games.csv")
+
+
+
 
 if __name__ == '__main__':
     main()
