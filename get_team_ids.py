d378c5ad7e37ee391d1ea41a59375618910ba88d
chic <thiagoiklock@gmail.com>
2023-07-20 21:58:58 -0300

    nothing to comment

---
+++ get_team_ids.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from nba_api.stats.static import teams
+
+# Get the list of NBA teams
+nba_teams = teams.get_teams()
+
+# Create a list of teams with the team name and team_id
+team_list = []
+for team in nba_teams:
+    team_list.append({
+        "name": team["full_name"],
+        "team_id": team["id"],
+    })
+    print(team["full_name"] , team["id"] )
+
+# Convert the team list to a Pandas DataFrame
+df = pd.DataFrame(team_list)
+
+# Save the DataFrame to a Parquet file
+df.to_parquet('team_list.parquet', index=False)
+ # Save the DataFrame to a JSON file
+save_json(df, "team_list.json")
--- play_by_play.py
+++ play_by_play.py
@@ -3,17 +3,12 @@ from nba_api.stats.static import teams
 
 nba_teams = teams.get_teams()
 
-# Select the dictionary for the Pacers, which contains their team ID
-pacers = [team for team in nba_teams if team['abbreviation'] == 'DAL'][0]
-pacers_id = pacers['id']
-print(f'pacers_id: {pacers_id}')
-
 # Query for the last regular season game where the Pacers were playing
 from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.library.parameters import Season
 from nba_api.stats.library.parameters import SeasonType
 
-gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=pacers_id,
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=1610612737,
                             season_nullable=Season.default,
                             season_type_nullable=SeasonType.regular)  
 
@@ -28,4 +23,5 @@ print(f'Searching through {len(games)} game(s) for the game_id of {game_id} wher
 # Query for the play by play of that most recent regular season game
 from nba_api.stats.endpoints import playbyplay
 df = playbyplay.PlayByPlay(game_id).get_data_frames()[0]
+df.to_csv("landing_areagame_details.csv")
 print(df.head()) #just looking at the head of the data
