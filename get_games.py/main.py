b71915e33cd42da6bd8dac0e9975becf10bbfd26
chic <thiagoiklock@gmail.com>
2023-07-20 22:47:05 -0300

    searching games

---
+++ get_games.py/main.py
@@ -0,0 +1,14 @@
+from nba_api.stats.static import teams
+
+nba_teams = teams.get_teams()
+# Select the dictionary for the Celtics, which contains their team ID
+celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
+celtics_id = celtics['id']
+
+from nba_api.stats.endpoints import leaguegamefinder
+
+# Query for games where the Celtics were playing
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
+# The first DataFrame of those returned is what we want.
+games = gamefinder.get_data_frames()[0]
+print(games.head())
